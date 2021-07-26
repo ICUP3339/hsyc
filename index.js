d4c59b2aa6115547b4b4ba1340cde26ddbff3302
BWXD <44984123+BWXD@users.noreply.github.com>
2021-07-26 18:54:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var client = new Discord.Client();
 
 client.on("ready", function() {
         client.user.setActivity(`test`,{type: "LISTENING"});
+	client.channels.cache.get("869262649580482641").send("Bot is up.")
 });
 
 client.on("message", async function(message) {
