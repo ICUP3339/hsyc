5cd0cde3420f2add700b8617348f97454dfad9b5
BWXD <44984123+BWXD@users.noreply.github.com>
2021-07-28 13:33:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,9 @@
-/*
 const Discord = require("discord.js");
 var client = new Discord.Client();
 
 client.on("ready", function() {
-        client.user.setActivity(`test`,{type: "LISTENING"});
-	client.channels.cache.get("869262649580482641").send("Bot is up.")
-});
-
-client.on("message", async function(message) {
-        if (message.author.equals(client.user)) return;
-        if (!message.content.startsWith(".")) return;
-        var args = message.content.substring(".".length).split(" ");
-        switch (args[0].toLowerCase()) {
-			
-			
-			
-            case "clear":
-                if(!args[1]) return;
-                message.delete()
-                message.channel.bulkDelete(args[1]);
-            break;
-			
-            case "help":
-			message.channel.send({embed: new Discord.RichEmbed()
-            			.setColor("#1b82cc")
-            			.setTitle("Feudal Clan Bot")
-				.addField(".ba", "Gives access for a clan owner/rep to a battle channel.")
-				.addField(".de", "Removes access from a clan owner/rep to a battle channel.")
-        		})
-            break;
-                        
-			
-			
-        }
-});
-
-
-
-client.login(process.env.TOKEN);
-*/
-
-const Discord = require("discord.js");
-var client = new Discord.Client();
-
-client.on("ready", function() {
-        console.log("Bot is up now.")
         client.user.setActivity(`Provoco chat | .help`,{type: "LISTENING"});
-        client.channels.cache.get("834559120148267018").send("Bot is up.")
+        client.channels.cache.get("869916380475064360").send("Bot is up!")
 });
 
 var tfcrole = "869310095706718340"
@@ -54,9 +11,17 @@ var fnrole = "869331552188891146"
 var wummyrole = "869303405460459520"
 var bdsmrole = "869373802205818921"
 var tdrrole = "869727243587440650"
+var rh = ""
+
+var clanname = "";
+var clanowner = "";
+var clandiscord = "";
+var clanlogo = "";
+var clancolor = "";
+//var clanmembers = ``;
 
 client.on("message", message => {
-    if(message.content.startsWith(".iam ") && !message.member.nickname.includes("Owner")){
+    if(message.content.startsWith(".iam ") && !message.member.roles.cache.has("869257733776482324")){
         
         if(message.content.split(".iam ") == ",TFC" && !message.member.roles.cache.has("869643087100657715")){
             message.member.setNickname("[TFC/Member] " + message.author.username)
@@ -126,52 +91,14 @@ client.on("message", message => {
             message.member.roles.add("868782125028098089")
             message.member.roles.remove("869304724309045378")
             message.member.roles.remove(tfcrole) //TFC
-            message.member.roles.remove("869609890220499034") //TSD
             message.member.roles.remove(fnrole) //FN
             message.member.roles.remove(wummyrole) //Wummy
             message.member.roles.remove(bdsmrole) //BDSM
+            message.member.roles.remove(tdrrole) //TDR
+        } else {
         }
-
-        else {
-        //if(message.content.split(".iam ") != "TFC" && message.content.split(".iam ") != "TSD" && message.content.split(".iam ") != "FN" && message.content.split(".iam ") != "Wummy" && message.content.split(".iam ") != "wummy" &&  message.content.split(".iam ") != "BDSM"){
-            //message.reply("this clan does not exist.")
-        //}
-        }
-        /*message.member.setNickname("[" + message.content.split(".iam ").slice(1,7) + "/Member] " + realNick)
-        message.reply("you have joined " + message.content.split(".iam ").slice(1,7))*/
-        /*if(message.content.split(".iam ").slice(1,7) == "FN"){
-            givenclanrole = fnrole
-            givenclanmember = "869304724309045378";
-        } if(message.content.split(".iam ").slice(1,7) == "TSD"){
-            givenclanrole = "869370650828087386"
-            givenclanmember = "869304724309045378";
-        } if(message.content.split(".iam ").slice(1,7) == "TFC"){
-            givenclanrole = tfcrole
-            givenclanmember = "869304724309045378";
-        } if(message.content.split(".iam ").slice(1,7) == "Wummy"){
-            givenclanrole = wummyrole
-            givenclanmember = "869304724309045378";
-        } if(message.content.split(".iam ").slice(1,7) == "BDSM"){
-            givenclanrole = bdsmrole
-            givenclanmember = "869304724309045378";
-        } if(message.content.split(".iam ").slice(1,7) != "TSD" && message.content.split(".iam ").slice(1,7) != "FN" && message.content.split(".iam ").slice(1,7) != "TFC" && message.content.split(".iam ").slice(1,7) != "wummy" && message.content.split(".iam ").slice(1,7) != "BDSM"){
-            givenclanrole = "868782125028098089"
-            givenclanmember = "868782125028098089";
-        }*/
-        /*message.member.roles.add(givenclanmember);
-        message.member.roles.remove("868782125028098089");
-        message.member.roles.add(givenclanrole);*/
-    } /*if(message.content.startsWith(".iam ") && !message.member.nickname.includes("Member")){
-        message.reply("you can't change clan as an Owner/Rep.")
-    }*/
+    }
 });
-
-var clanname = "";
-var clanowner = "";
-var clandiscord = "";
-var clanlogo = "";
-var clancolor = "";
-var clanmembers = ``;
 
 client.on("message", async function(message) {
 if (message.author.equals(client.user)) return;
@@ -187,26 +114,36 @@ switch (args[0].toLowerCase()) {
     // HELP COMMANDS
 			
     case "help":
-        const embedaaa = new Discord.MessageEmbed()
+        const embed1 = new Discord.MessageEmbed()
             .setColor("#1b82cc")
             .setTitle("Provoco Clan Bot: Commands")
             .addField(".iam [Clan]", "Command to join a clan.")
             .addField(".claninfo [Clan]", "Displays informations about a clan.")
             .addField(".clans", "Shows list of currently registered clans.")
             .addField(".ping", "Gives bot's exact ping latency.")
-        message.channel.send(embedaaa)
+        message.channel.send(embed1)
+    break;
+
+    case "cohelp":
+        if(message.member.roles.cache.has("869232215958097960")){
+            const embed2 = new Discord.MessageEmbed()
+                .setColor("#1b82cc")
+                .setTitle("Provoco Clan Bot: Clan Owner")
+                .addField(".setrep [User]", "Sets a member as a Rep in your clan.")
+            message.channel.send(embed2)
+        }
     break;
 			
     case "staffhelp":
         if(message.member.roles.cache.has("869232215958097960")){
-            const embedaab = new Discord.MessageEmbed()
+            const embed3 = new Discord.MessageEmbed()
                 .setColor("#1b82cc")
-                .setTitle("Provoco Clan Bot: Moderators")
+                .setTitle("Provoco Clan Bot: Staff")
                 .addField(".ba :warning:", "Gives access for a clan owner/rep to a battle channel.")
                 .addField(".de :warning:", "Removes access from a clan owner/rep to a battle channel.")
                 .addField(".verify [User]", "Confirms an unverified user.")
                 .addField(".clear [Number]", "Purges a defined amount of messages.")
-            message.channel.send(embedaab)
+            message.channel.send(embed3)
         }
     break;
 
@@ -252,13 +189,13 @@ switch (args[0].toLowerCase()) {
             clanlogo = "https://cdn.discordapp.com/attachments/869743818998702101/869743873419800626/TDR_SIGN.png"
         }
 
-        const newEmbeda = new Discord.MessageEmbed()
+        const embed4 = new Discord.MessageEmbed()
             .setColor("#" + clancolor)
             .setTitle("" + clanname + "")
             .addField("**Clan Owner:**",clanowner)
             .addField("**Clan Server:**",clandiscord)
             .setThumbnail(clanlogo)
-        message.channel.send(newEmbeda)
+        message.channel.send(embed4)
 
         clancolor = ""
         clanname = ""
@@ -277,8 +214,47 @@ switch (args[0].toLowerCase()) {
     break;
 
 
-    // STAFF COMMANDS
+    // CLAN OWNERS COMMANDS
 
+    case "setrep":
+        var a = message.mentions.members.first();
+        if(message.member.roles.cache.has("869257733776482324") || message.member.roles.cache.has("869232215958097960" && a)){
+            message.delete();
+            if(message.member.roles.cache.has(tdrrole) && a.roles.cache.has(tdrrole)){
+                rh = "TDR"
+                a.roles.add("869257648875380816")
+                a.roles.remove("869304724309045378")
+                a.setNickname("[" + rh + "/Rep]")
+            }
+            if(message.member.roles.cache.has(bdsmrole) && a.roles.cache.has(bdsmrole)){
+                rh = "BDSM"
+                a.roles.add("869257648875380816")
+                a.roles.remove("869304724309045378")
+                a.setNickname("[" + rh + "/Rep]")
+            }
+            if(message.member.roles.cache.has(fnrole) && a.roles.cache.has(fnrole)){
+                rh = "FN"
+                a.roles.add("869257648875380816")
+                a.roles.remove("869304724309045378")
+                a.setNickname("[" + rh + "/Rep]")
+            }
+            if(message.member.roles.cache.has(wummyrole) && a.roles.cache.has(wummyrole)){
+                rh = "Wummy"
+                a.roles.add("869257648875380816")
+                a.roles.remove("869304724309045378")
+                a.setNickname("[" + rh + "/Rep]")
+            }
+            if(message.member.roles.cache.has(tfcrole) && a.roles.cache.has(tfcrole)){
+                rh = "TFC"
+                a.roles.add("869257648875380816")
+                a.roles.remove("869304724309045378")
+                a.setNickname("[" + rh + "/Rep]")
+            }
+        }
+    break;
+
+
+    // STAFF COMMANDS
     
     case "ba":
         if(message.member.roles.cache.has("869232215958097960")){
@@ -293,12 +269,12 @@ switch (args[0].toLowerCase()) {
     break;
 
     case "verify":
-    var member = message.mentions.members.first();    
-    if(message.member.roles.cache.has("869232215958097960") && member){
+    var unv = message.mentions.members.first();    
+    if(message.member.roles.cache.has("869232215958097960") && unv){
         message.delete();
-        member.roles.add("868782125028098089")
-        member.roles.remove("869237254466445353")
-        member.setNickname("[Clanless] " + member.user.username)
+        unv.roles.add("868782125028098089")
+        unv.roles.remove("869237254466445353")
+        unv.setNickname("[Clanless] " + member.user.username)
         message.channel.send("<@" + member + "> has been verified!")
     }
     break;
@@ -362,12 +338,12 @@ switch (args[0].toLowerCase()) {
             clanlogo = "https://cdn.discordapp.com/attachments/869743818998702101/869743873419800626/TDR_SIGN.png"
         }
 
-        const newEmbeda1 = new Discord.MessageEmbed()
+        const embedA5 = new Discord.MessageEmbed()
             .setColor("#" + clancolor)
             .setTitle("" + clanname + "")
             .setDescription(clanmembers)
             .setThumbnail(clanlogo)
-        message.channel.send(newEmbeda1)
+        message.channel.send(embedA5)
 
         clancolor = ""
         clanname  = ""
